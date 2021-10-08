cc08ecaf77dc3124344d7a1f37291fcab68083cd
Madejczyk <jakub.madejczyk92@gmail.com>
2021-10-08 23:04:00 +0200

    Add react

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export const App = ({ callback }) => {
+    return (
+        <>
+            <h1>Hello World</h1>
+        </>
+    );
+}
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: './src/app.js',
+  entry: './src/main.jsx',
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: 'bundle.js',
