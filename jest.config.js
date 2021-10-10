109781c6d8f46ded7fec9c5b60c38aa30a0fecbb
Jakub Madejczyk <Madejczyk@users.noreply.github.com>
2021-10-10 12:08:46 +0200

    #42 Add threshold (#44)

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  coverageThreshold: {
+    global: {
+      branches: 65,
+      functions: 85,
+      lines: 85,
+      statements: 85,
+    },
+  },
   testEnvironment: 'jsdom',
   transform: {
     '^.+\\.(t|j)sx?$': ['@swc/jest'],
