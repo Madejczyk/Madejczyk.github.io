191c84eb149c30d92cade87e73184ebb0c5b97e8
Madejczyk <jakub.madejczyk92@gmail.com>
2021-10-11 23:25:45 +0200

    fix by prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,12 @@
 module.exports = {
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:jest/recommended",
-        "plugin:react/recommended",
-        "plugin:react-hooks/recommended",
-        "plugin:prettier/recommended"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "plugins": [
-        "@typescript-eslint",
-        "jest",
-        "react",
-        "react-hooks",
-        "prettier"
-    ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'jest', 'react', 'react-hooks', 'prettier'],
 }
