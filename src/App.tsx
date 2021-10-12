8a5f9879eb12989388cbefdee98a99cb186398b2
Madejczyk <jakub.madejczyk92@gmail.com>
2021-10-12 19:13:34 +0200

    Add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import * as React from 'react'
 
 const App = () => {
   return (
-    <div>
-      <h1>I am also waiting for this...</h1>
+    <div className="flex justify-center">
+      <p>I am also waiting for this...</p>
     </div>
   )
 }
