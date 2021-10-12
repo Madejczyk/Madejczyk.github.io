1b04667e303f512bfc43c54184d9c49ca17cc3c3
Jakub Madejczyk <Madejczyk@users.noreply.github.com>
2021-10-12 19:17:00 +0200

    Add tailwind (#53)

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -7,10 +7,12 @@ test('renders a message', () => {
   const {container, getByText} = render(<App />)
   expect(getByText('I am also waiting for this...')).toBeInTheDocument()
   expect(container.firstChild).toMatchInlineSnapshot(`
-  <div>
-    <h1>
+  <div
+    class="flex justify-center"
+  >
+    <p>
       I am also waiting for this...
-    </h1>
+    </p>
   </div>
   `)
 })
